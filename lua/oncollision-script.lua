1a9cc45e951dfb21f7556bca6fdd2c55b1473021
ChainSwordCS <chainswordcs@gmail.com>
2026-09-12 07:49:09 -0400

    Update oncollision-script.lua
    update collision view at 30 fps (or whatever rate the engine is updating at), not 60

--- lua/oncollision-script.lua
+++ lua/oncollision-script.lua
@@ -58,10 +58,57 @@ function readfixedpoint2012(ptr)
 	return (dword / 4096.0)
 end
 
+-- tracks whether the current frame of emulated nds execution is a new frame just rendered and presented by the 3D Engine,
+-- (true)
+-- or if it's just a duplicate (nds runs at 60 hz, this game redraws the 3D scene at 30 hz maximum), or a lag frame
+-- (false)
+-- not to be confused with desmume emulator GUI update frames
+isNew3dEngineFrame = true
+isNew3dEngineFrame2 = true -- one frame delayed
+isNew3dEngineFrame3 = true -- two frames delayed
+_frame_count_prev_value = -1
+function checkIsNew3dEngineFrame()
+	isNew3dEngineFrame3 = isNew3dEngineFrame2
+	isNew3dEngineFrame2 = isNew3dEngineFrame
+	-- potential bytes in memory to try, for tracking this
+	-- (which increment on every new frame rendered and presented by the 3D Engine,
+	-- and which do not increment on off-frames (30 fps) or lag frames.)
+	-- (DJTS beta debug build only. offsets will be different from retail build.)
+	--02106ab8
+	--021090a8
+	--02121194
+	--021547d2
+	--021548b2
+	--02154992
+	--02154a72
+	--02266b68
+	--02266f48
+	--0227f9f8
+	--02360da0
+	--02360df8
+	--02361380
+	-- same as above, but paused during bottom-screen dialogue
+	--021dcbec
+	--023615dc
+	--02361620
+	--023616ac
+	--023616f0
+	--02361800
+	--0236184c
+	a = memory.readbyte(0x02106ab8)
+	if a == _frame_count_prev_value then
+		isNew3dEngineFrame = false
+	else
+		_frame_count_prev_value = a
+		isNew3dEngineFrame = true
+	end
+	return
+end
 
 const_vp_x_fac = 256.0 / const_vp_w
 const_vp_z_fac = 192.0 / const_vp_h
 
+_drawCollision_prevFrameRectList = { {0,0,0,0}, {0,0,0,0} }
 -- simple and hacky. only designed to work with certain camera hacks in play.
 function drawOverlay()
 	cam_ptr = memory.readdword(locCamPtr)
@@ -69,35 +116,39 @@ function drawOverlay()
 		-- probably not in-game; abort!
 		return
 	end
-	
 	gui.opacity(0.5)
-	
-	cam_x = readfixedpoint2012(cam_ptr + 0x90)
-	cam_z = readfixedpoint2012(cam_ptr + 0x98)
-	
-	-- 4:3 aspect
-	-- "vp" stands for "viewport" btw
-	vp_topleft  = { cam_x-const_vp_w/2.0, cam_z-const_vp_h/2.0 }
-	vp_botright = { cam_x+const_vp_w/2.0, cam_z+const_vp_h/2.0 }
-	
-	for _, c in pairs(unique_contacts) do
-		-- test that both points are within view
-		--if (1 == 1) then
-		if (c[2] > vp_topleft[1] and c[2] < vp_botright[1]) -- x1
-		and (c[4] > vp_topleft[2] and c[4] < vp_botright[2]) -- z1
-		and (c[5] > vp_topleft[1] and c[5] < vp_botright[1]) -- x2
-		and (c[7] > vp_topleft[2] and c[7] < vp_botright[2]) then -- z2
-			-- map onto new coord plane
-			x1 = (c[2] * const_vp_x_fac) - (vp_topleft[1] * const_vp_x_fac)
-			z1 = (c[4] * const_vp_z_fac) - (vp_topleft[2] * const_vp_z_fac)
-			x2 = (c[5] * const_vp_x_fac) - (vp_topleft[1] * const_vp_x_fac)
-			z2 = (c[7] * const_vp_z_fac) - (vp_topleft[2] * const_vp_z_fac)
-			-- subtract 192 from the y value to draw on the top screen
-			gui.drawrect(x1, z1-192, x2, z2-192)
-			print(string.format("debug: drawrect(%.1f, %.1f, %.1f, %.1f)",x1,z1-192,x2,z2-192))
+	if isNew3dEngineFrame3 and not emu.paused then
+		-- refresh
+		_drawCollision_prevFrameRectList = { }
+		cam_x = readfixedpoint2012(cam_ptr + 0x90)
+		cam_z = readfixedpoint2012(cam_ptr + 0x98)
+		vp_topleft  = { cam_x-const_vp_w/2.0, cam_z-const_vp_h/2.0 }
+		vp_botright = { cam_x+const_vp_w/2.0, cam_z+const_vp_h/2.0 }
+		for _, c in pairs(unique_contacts) do
+			-- test that both points are within view
+			--if (1 == 1) then
+			if (c[2] > vp_topleft[1] and c[2] < vp_botright[1]) -- x1
+			and (c[4] > vp_topleft[2] and c[4] < vp_botright[2]) -- z1
+			and (c[5] > vp_topleft[1] and c[5] < vp_botright[1]) -- x2
+			and (c[7] > vp_topleft[2] and c[7] < vp_botright[2]) then -- z2
+				-- map onto new coord plane
+				x1 = (c[2] * const_vp_x_fac) - (vp_topleft[1] * const_vp_x_fac)
+				z1 = (c[4] * const_vp_z_fac) - (vp_topleft[2] * const_vp_z_fac)
+				x2 = (c[5] * const_vp_x_fac) - (vp_topleft[1] * const_vp_x_fac)
+				z2 = (c[7] * const_vp_z_fac) - (vp_topleft[2] * const_vp_z_fac)
+				-- subtract 192 from the y value to draw on the top screen
+				gui.drawrect(x1, z1-192, x2, z2-192)
+				--print(string.format("debug: drawrect(%.1f, %.1f, %.1f, %.1f)",x1,z1-192,x2,z2-192))
+				table.insert(_drawCollision_prevFrameRectList, {x1, z1, x2, z2})
+			end
+		end
+	else
+		-- redraw the same stuff we drew last frame
+		for _, rect in pairs(_drawCollision_prevFrameRectList) do
+			gui.drawrect(rect[1], rect[2]-192, rect[3], rect[4]-192)
+			--print(string.format("debug: drawrect(%.1f, %.1f, %.1f, %.1f)",x1,z1-192,x2,z2-192))
 		end
 	end
-	
 	gui.opacity(1.0) -- return to normal afterwards
 	return
 end
@@ -186,6 +237,7 @@ end
 
 -- Main loop ----------------------------------------------------------------------------------------------------------
 while true do
+	checkIsNew3dEngineFrame()
 	update()
 	emu.frameadvance()
 end
