ebef343caac1a7bfa40dd8dbc921fb9d93379e45
ChainSwordCS <chainswordcs@gmail.com>
2026-09-16 22:39:50 -0400

    Update ortho-test.lua

--- lua/ortho-test.lua
+++ lua/ortho-test.lua
@@ -8,41 +8,90 @@ print("hello world")
 
 -- (doesn't work :P)
 
+function mtx_callbackM1()
+	r0 = memory.getregister("r0");
+	print(string.format("mtx_callbackM1 variable = %08X", r0));
+	print(string.format("param_1=%08X", memory.readdword(r0 + 0x38)));
+	print(string.format("param_2=%08X", memory.readdword(r0 + 0x3c)));
+	print(string.format("param_3=    %04X", memory.readword(r0 + 0x40)));
+	print(string.format("param_4=%08X", memory.readdword(r0 + 0x44)));
+	print(string.format("param_5=%08X", memory.readdword(r0 + 0x48)));
+end
+
+-- written weird to work around some odd behavior
+function printregisters()
+	print(string.format("class_instance_ptr=%08X", memory.getregister("r4")));
+	print(string.format("param_1=%08X", memory.getregister("r0")));
+	print(string.format("param_2=%08X", memory.getregister("r1")));
+	print(string.format("param_3=%08X", memory.getregister("r2")));
+	print(string.format("param_4=%08X", memory.getregister("r3")));
+	--r4 = memory.getregister("r4");
+	--print(string.format("param_5=%08X", memory.readdword(r4 + 0x48)));
+	sp = memory.getregister("r13");
+	print(string.format("param_5=%08X", memory.readdword(sp + 0x00)));
+	print(string.format("param_6=%08X", memory.readdword(sp + 0x04)));
+	print(string.format("param_7=%08X", memory.readdword(sp + 0x08)));
+	print(string.format("param_8=%08X", memory.readdword(sp + 0x0c)));
+	
+	print(string.format("sp=%08X", memory.getregister("r13")));
+	print(string.format("lr=%08X", memory.getregister("r14")));
+	print(string.format("pc=%08X", memory.getregister("r15")));--why is it apparently off by 8?
+end
+
 -- hack param_7 to set it to 1, not 0
 function mtx_callback0()
-	--memory.setregister("r6", 1);
+	print("");
+	print("FUN_020ba13c");
+	printregisters();
+	print("mtx_callback0");
 	sp = memory.getregister("r13");
-	memory.writedword(sp + 0x08, 1);
-	print("param_7="..memory.readdword(sp + 0x08));
+	param_7 = memory.readdword(sp + 0x08);
+	if (param_7 == 0) then
+		const_enable = 1;
+		--memory.setregister("r6", const_enable);
+		memory.writedword(sp + 0x08, const_enable);
+	end
 end
 
 function mtx_callback1()
-	--print("hi");
+	print("1");
 	mtx_1 = memory.getregister("r0");
-	memory.setregister("r0", mtx_1 / (15.0 * 4096.0));
+	a = math.ceil(((mtx_1 / 4096.0) / 15.0) * 4096.0);
+	memory.setregister("r0", a);
+	
 	mtx_6 = memory.getregister("r7");
-	memory.setregister("r7", mtx_6 / (15.0 * 4096.0));
+	b = math.ceil(((mtx_6 / 4096.0) / 15.0) * 4096.0);
+	memory.setregister("r7", b);
 end
 
 function mtx_callback2()
+	print("2");
 	mtx_11 = memory.getregister("r7");
-	memory.setregister("r7", mtx_11 / (17.15 * 4096.0));
+	c = math.ceil(((mtx_11 / 4096.0) / 17.15) * 4096.0);
+	memory.setregister("r7", c);
+	
 	--mtx_12 = memory.getregister("r6");
-	memory.setregister("r6", (0.00025 * 4096.0)); -- the game's code multiplies this by -1, btw
+	d = math.ceil(0.00025 * 4096.0);
+	memory.setregister("r6", d); -- the game's code multiplies this by -1, btw
+	
 	mtx_15 = memory.getregister("r2");
-	memory.setregister("r2", mtx_15 * (10 * 4096.0));
+	e = math.ceil(((mtx_15 / 4096.0) * 10) * 4096.0);
+	memory.setregister("r2", e);
 end
 
 function mtx_callback3()
-	mtx_16 = memory.getregister("r0");
-	memory.setregister("r0", (1.0 * 4096.0));
+	print("3");
+	--mtx_16 = memory.getregister("r0");
+	f = math.ceil(1.0 * 4096.0);
+	memory.setregister("r0", f);
 	
 	-- is this necessary?
 	r5 = memory.getregister("r5");
-	memory.writedword(r5 + 0x3c, (1.0 * 4096.0));
+	memory.writedword(r5 + 0x3c, f);
 end
 
+--memory.registerexec(0x02097ed4, mtx_callbackM1);
 memory.registerexec(0x020ba13c, mtx_callback0);
-memory.registerexec(0x020ba21c, mtx_callback1);
-memory.registerexec(0x020ba2e8, mtx_callback2);
-memory.registerexec(0x020ba300, mtx_callback3);
+memory.registerexec(0x020ba220, mtx_callback1);
+memory.registerexec(0x020ba2ec, mtx_callback2);
+memory.registerexec(0x020ba304, mtx_callback3);
